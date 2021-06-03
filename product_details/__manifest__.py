a273efbf3f5becefb1c8f895f0f5d34d0cfa6622
elsayedh <elsayedh@hotmail.com>
2021-06-03 10:16:17 +0300

    Add files via upload

--- product_details/__manifest__.py
+++ product_details/__manifest__.py
@@ -20,13 +20,15 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','product',],
+    'depends': ['base','product','stock'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'data/product_category_data.xml',
+
     ],
     # only loaded in demonstration mode
     'demo': [
--- product_details/models/models.py
+++ product_details/models/models.py
@@ -2,19 +2,57 @@
 
 from odoo import api, fields, models, exceptions, _
 
+
 class ProductCategory(models.Model):
     _inherit = 'product.category'
 
     name = fields.Char('Name', index=True, required=True, translate=True)
 
+    @api.depends('name')
+    def name_get(self):
+        print("hi",self.env.lang)
+        res = []
+        for record in self:
+            name =  record.name
+            print("name",record.name)
+            res.append((record.id, name))
+        return res
 
+class ProductTemplatedetails(models.Model):
+    _name='product.color'
+    name = fields.Char('Color', index=True, translate=True)
+
+    @api.depends('name')
+    def name_get(self):
+        print("hi", self.env.lang)
+        res = []
+        for record in self:
+            name = record.name
+            print("name", record.name)
+            res.append((record.id, name))
+        return res
+
+class ProductTemplatedetailsb(models.Model):
+    _name='product.brand'
+
+    name = fields.Char('Brand', index=True, translate=True)
+
+    @api.depends('name')
+    def name_get(self):
+        print("hi", self.env.lang)
+        res = []
+        for record in self:
+            name = record.name
+            print("name", record.name)
+            res.append((record.id, name))
+        return res
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
+    color_id = fields.Many2one('product.color', 'Color',  ondelete='cascade')
+    brand_id = fields.Many2one('product.brand', 'Brand',ondelete='cascade')
 
-    color = fields.Char('Color', index=True,   translate=True)
-    brand = fields.Char('Brand', index=True,   translate=True)
 
 
 
